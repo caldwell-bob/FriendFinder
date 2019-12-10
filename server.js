6cfabf7bbd04596c6810eae1aca9d36cec6c772a
Bob Caldwell <themaccat@gmail.com>
2019-12-09 21:56:29 -0500

    fixed some routes, updated from to jquery, added modal for results..major fix

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
 
 // create app/json parser
-var jsonParser = bodyParser.json();
+// var jsonParser = bodyParser.json();
+// ! - this is the root of all my issues I think.
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+// require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json({ type:  'application/*+json' }));
+// app.use(bodyParser.raw({ type:  'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type:  'text/html' }));
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
-
-app.use(bodyParser.json({ type:  'application/*+json' }));
-app.use(bodyParser.raw({ type:  'application/vnd.custom-type' }));
-app.use(bodyParser.text({ type:  'text/html' }));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
